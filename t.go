1b803b57f487757e0327574a531d27e1cb621ab2
cxllwel <159736268+cxllwel@users.noreply.github.com>
2024-11-02 14:38:52 +0700

    Update t.go

--- t.go
+++ t.go
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
 package main
 
 import (
