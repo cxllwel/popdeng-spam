0a0857ea46f579612fd32836225c40bb25fbd41f
cxllwel <159736268+cxllwel@users.noreply.github.com>
2024-12-13 22:32:42 +0700

    Update m.go

--- m.go
+++ m.go
@@ -85,7 +85,7 @@ func Connect() {
 		}
 
 		// client.Subscribe("popdeng/leaderboard", 1, nil)
-		// client.Subscribe("popdeng/clicks", 1, nil)
+		client.Subscribe("popdeng/clicks", 1, nil)
 		quitT := time.Now().Add(Delay)
 		for time.Now().Before(quitT) {
 			// token := client.Publish("popdeng/leaderboard", 0, false, `[{"country_code":"WW","clicks":0,"cps":0},{"country_code":"KP","clicks":6969696969,"cps":6969696969},{"country_code":"IS","clicks":6969696969,"cps":6969696969},{"country_code":"WW","clicks":0,"cps":0},{"country_code":"HK","clicks":69,"cps":1},{"country_code":"AT","clicks":69,"cps":2},{"country_code":"CN","clicks":69,"cps":3},{"country_code":"KW","clicks":69,"cps":4},{"country_code":"ZZ","clicks":0,"cps":0},{"country_code":"BE","clicks":69,"cps":5},{"country_code":"YE","clicks":69,"cps":6},{"country_code":"ZZ","clicks":0,"cps":0},{"country_code":"LR","clicks":69,"cps":7},{"country_code":"AQ","clicks":69,"cps":8},{"country_code":"RO","clicks":69,"cps":9},{"country_code":"IS","clicks":69,"cps":10},{"country_code":"NO","clicks":69,"cps":11},{"country_code":"AO","clicks":69,"cps":12},{"country_code":"ZZ","clicks":0,"cps":0},{"country_code":"TH","clicks":1000,"cps":1},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]`)
@@ -101,7 +101,7 @@ func Connect() {
 }
 
 func main() {
-	for i := 0; i < 500; i++ {
+	for i := 0; i < 20; i++ {
 		go Connect()
 		time.Sleep(time.Millisecond * 25)
 	}
